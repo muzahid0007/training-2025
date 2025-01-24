7d87a9b77b041147098e0c3849d84db9558e82d0
muzahid0007 <158133391+muzahid0007@users.noreply.github.com>
2025-01-24 11:42:21 +0530

    Add files via upload

---
+++ day 1/dom.js
@@ -0,0 +1,46 @@
+// let ele = document.getElementsByClassName("test");
+// ele[0].style.backgroundColor="dodgerblue"
+// console.log(ele);
+// console.log(ele);
+// console.log(Array.isArray(ele));
+
+//! spread operator :- this is used to take out each and every value from
+//! original array and store it in one more new array (pure arrat)
+
+
+
+
+ /* let x= [...ele];
+ console.log(x,Array.isArray(x));
+ x.map(element=>{
+    element.style.backgroundColor="tomato";
+    element.style.color="black";
+    element.style.textTransform="uppercase";
+ })
+
+ */
+
+ //!
+ /* let ele = document.getElementsByTagName("test"); */
+
+ //!
+ /* let ele = document.getElementsByName("username");
+ [...ele].map(u=>{
+    console.log(u,)
+ })
+*/
+
+//!
+ /* let ele = document.querySelector("#demo");
+ console.log(ele) */
+ /* let ele = document.querySelector(".test");
+ console.log(ele)
+ */
+
+ /* let ele = document.querySelectorAll(".test");
+ console.log(ele)
+
+ */
+
+
+
